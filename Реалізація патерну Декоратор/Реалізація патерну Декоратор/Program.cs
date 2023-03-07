b620cec84f93e7bcb20529d013b671b4746d3c1e
Kukuruzka742 <124073842+Kukuruzka742@users.noreply.github.com>
2023-03-07 09:53:32 +0200

    Add files via upload

---
+++ Реалізація патерну Декоратор/Реалізація патерну Декоратор/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Реалізація_патерну_Декоратор
+{
+
+    internal class Program
+    {
+        public abstract class ArrayDecorator<T>
+        {
+            protected T[] mass;
+            public ArrayDecorator(T[] mass)
+            { 
+                this.mass = mass;
+            }
+            public abstract void Print();
+        }
+
+        public class ConsolArray<T> : ArrayDecorator<T>
+        {
+            public ConsolArray(T[] mass) : base(mass) {  }
+            public override void Print()
+            {
+                foreach (T t in mass)
+                {
+                    Console.Write(t + " ");
+                }
+            }
+        }
+
+        public class VisualArray<T> : ArrayDecorator<T>
+        {
+            public VisualArray(T[] mass) : base(mass) { }
+            public override void Print()
+            {
+                Form1 f1 = new Form1();
+                
+                foreach (T t in mass)
+                {
+                    f1.textBox1.Text += t + " ";
+                }
+                f1.textBox1.ReadOnly = true;
+                f1.ShowDialog();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int SIZE = (Convert.ToInt32(Console.ReadLine()));
+            int[] mass = Enumerable.Range(0, SIZE).ToArray();
+
+            ArrayDecorator<int> ca = new ConsolArray<int>(mass);
+            ca.Print();
+            ca = new VisualArray<int>(mass);
+            ca.Print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Реалізація патерну Декоратор/Реалізація патерну Декоратор/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Реалізація патерну Декоратор")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Реалізація патерну Декоратор")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4b6ced9f-5783-4bed-8969-60b96163a4c4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
