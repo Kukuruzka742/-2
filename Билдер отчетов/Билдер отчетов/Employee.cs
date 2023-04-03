97a5e4fde0bc4fff3e48f33a1226f34596cb9230
Kukuruzka742 <124073842+Kukuruzka742@users.noreply.github.com>
2023-04-03 20:21:11 +0300

    Add files via upload

---
+++ Билдер отчетов/Билдер отчетов/Employee.cs
@@ -0,0 +1,116 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Билдер_отчетов
+{
+    // інтерфейс будівельника звіту
+    interface IReportBuilder
+    {
+        void SetHeader(string header);
+        void SetBody(string body);
+        void SetFooter(string footer);
+        string GetReport();
+    }
+
+    // конкретний будівельник звіту у вигляді текстового документу
+    class TextReportBuilder : IReportBuilder
+    {
+        private string _report;
+
+        public void SetHeader(string header)
+        {
+            _report += "Header: " + header + "\n";
+        }
+
+        public void SetBody(string body)
+        {
+            _report += "Body: " + body + "\n";
+        }
+
+        public void SetFooter(string footer)
+        {
+            _report += "Footer: " + footer + "\n";
+        }
+
+        public string GetReport()
+        {
+            return _report;
+        }
+    }
+
+    class HTMLReportBuilder : IReportBuilder
+    {
+        private string _report;
+
+        public HTMLReportBuilder()
+        {
+            _report += "<html><style>.box {\r\n  display: flex;\r\n  flex-direction: column;\r\n align-items: center;\r\n  justify-content: space-between;\r\n  height: 500px;\r\n}</style> <body><div class=\"box\">";
+        }
+
+        public void SetHeader(string header)
+        {
+            _report += "<header>\n<h1>\n" + header + "</h1>\n</header>\n";
+        }
+
+        public void SetBody(string body)
+        {
+            _report += "<main>\n<h5>\n" + body + "</h5>\n</main>\n";
+        }
+
+        public void SetFooter(string footer)
+        {
+            _report += "<footer>\n<h1>\n" + footer + "</h1>\n</footer>\n";
+        }
+
+        public string GetReport()
+        {
+            _report += "</div></body></html>";
+            return _report;
+        }
+    }
+
+    // директор, який складає звіти з відповідною послідовністю етапів
+    class ReportDirector
+    {
+        private IReportBuilder _builder;
+
+        public ReportDirector(IReportBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void ConstructReport(string header, string body, string footer)
+        {
+            _builder.SetHeader(header);
+            _builder.SetBody(body);
+            _builder.SetFooter(footer);
+        }
+
+        public string GetReport()
+        {
+            return _builder.GetReport();
+        }
+    }
+
+    class HTMLReportDirector
+    {
+        private IReportBuilder _builder;
+
+        public HTMLReportDirector(IReportBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void ConstructReport(string header, string body, string footer)
+        {
+            _builder.SetHeader(header);
+            _builder.SetBody(body);
+            _builder.SetFooter(footer);
+        }
+
+        public string GetReport()
+        {
+            return _builder.GetReport();
+        }
+    }
+}
---
+++ Билдер отчетов/Билдер отчетов/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Билдер_отчетов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            ReportDirector director = new ReportDirector(new TextReportBuilder());
+            HTMLReportDirector htmldirector = new HTMLReportDirector(new HTMLReportBuilder());
+
+            //------------------------------------------------------------------------
+
+            director.ConstructReport("Заголовок звіту", "Текст звіту", "Підпис");
+            string report = director.GetReport();
+
+            Console.WriteLine(report);
+            Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter("txt.txt", false))
+            {
+                writer.WriteLine(report);
+            }
+            string filePath = "txt.txt";
+            Process.Start(filePath);
+
+            //-------------------------------------------------------------------------
+
+            htmldirector.ConstructReport("Заголовок звіту", "Текст звіту", "Підпис");
+            report = htmldirector.GetReport();
+
+            Console.WriteLine(report);
+            Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter("index.html", false))
+            {
+                writer.WriteLine(report);
+            }
+            filePath = "index.html";
+            Process.Start(filePath);
+
+        }
+    }
+}
